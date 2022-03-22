0f5cbb75362ba79912e9f317aa331576aeae7d06
Your Name <you@example.com>
2022-03-23 01:30:18 +0300

    deploy.sh home laptop

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 make
 mv /home/ilit/VsProjects/factorio-ambient-seasons/pkg/*.zip /home/ilit/.factorio/mods/
+mv /home/ilit/IdeaProjects/factorio-ambient-seasons/pkg/*.zip /home/ilit/.factorio/mods/
