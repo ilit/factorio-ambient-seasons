76f2417483c35f61b84f30dade5ead10f7068236
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-03-23 17:59:29 +0300

    refact

--- code/season-step.lua
+++ code/season-step.lua
@@ -1,24 +1,5 @@
 require "noise"
-
--- Normalize to [0-1] scale
-function normalize(rawNoiseValsToPos)
-    local TOP_ELEVATION = 27
-
-    local ret = {}
-    for i, triplet in ipairs(rawNoiseValsToPos) do
-        local rawNoise = triplet[1]
-        local capped = math.min(rawNoise, TOP_ELEVATION)
-        local normalized = capped / TOP_ELEVATION
-
-        local x = triplet[2]
-        local y = triplet[3]
-
-        ret[#ret +1] =
-        { normalized, x, y }
-    end
-
-    return ret
-end
+normalize = require "code/noise/normalize"
 
 -- Filter positions by noise
 function filterPositionsByNoise(valsToPos, min, max)
