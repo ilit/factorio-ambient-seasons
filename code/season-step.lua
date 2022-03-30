7175d3d660adbe2b141d3ddf41fbfd2d4201a425
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-03-30 19:32:34 +0300

    todo

--- code/season-step.lua
+++ code/season-step.lua
@@ -1,6 +1,8 @@
 local writeSeasonOverChunk = require "code/procedures/write-season-over-chunk.lua"
 
 return function()
+    -- TODO migrate to chunk_charted
+    -- TODO migrate to job-queue
     for chunk in game.surfaces.nauvis.get_chunks() do
         if game.surfaces.nauvis.is_chunk_generated(chunk) then
             writeSeasonOverChunk(chunk)
