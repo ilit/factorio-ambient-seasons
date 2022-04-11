d9f78c6d2f5c468aad1dc6aab97093334e102fcc
Your Name <you@example.com>
2022-04-12 02:38:45 +0300

    disable variation for test editor

--- ent-test-scratch.lua
+++ ent-test-scratch.lua
@@ -1,10 +1,10 @@
 local surface = game.player.surface
 local entities = surface.find_entities_filtered{area = area, type = 'tree'}
 for _, e in pairs(entities) do
-    if e.name == "tree-03" then
-        --e.tree_color_index = 3
-        --e.graphics_variation = 1
-        e.tree_stage_index = 2
+    if e.name then
+        e.tree_color_index = 1
+        e.graphics_variation = 1
+        --e.tree_stage_index = 1
         --e.tree_gray_stage_index = 1 -- 1 to 15
     end
 end
