7aee4211884f1220e3a8ee9673c65a06953bd405
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-04-12 14:45:25 +0300

    scratch dir, codes

---
+++ data/codes-coniferous.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "tree-01",
+        "tree-dryland-j",
+        "tree-dryland-l",
+        "tree-dryland-o",
+        "tree-wetland-o",
+    },
+    {
+        "tree-dryland-o",
+    },
+    {
+        "tree-snow-a",
+    }
+}
--- scratch/ent-test-scratch.lua
+++ scratch/ent-test-scratch.lua
@@ -1,5 +1,6 @@
 local surface = game.player.surface
-local entities = surface.find_entities_filtered{area = area, type = 'tree'}
+local entities = surface.find_entities_filtered { area = area, type = 'tree' }
+
 for _, e in pairs(entities) do
     if e.name then
         e.tree_color_index = 1
@@ -8,5 +9,7 @@ for _, e in pairs(entities) do
         --e.tree_gray_stage_index = 1 -- 1 to 15
     end
 end
+
+
 log(serpent.block(data.raw.tile["frozen-snow-1"]))
 game.print(serpent.block(data.raw.tile["frozen-snow-1"]))
