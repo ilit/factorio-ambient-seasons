b3f5d173be0f9094d6654a2cedac16afd3adf48b
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-04-25 21:32:37 +0300

    comments

--- code/functions-lib/utils/get-pos-noise-from-chunk.lua
+++ code/functions-lib/utils/get-pos-noise-from-chunk.lua
@@ -1,3 +1,8 @@
+--- pos position to find index in noiseX, noiseY
+--- noiseVals array with index
+--- noiseX array with index
+--- noiseY array with index
+--- returns noise value from noiseVals by index
 return function(pos, noiseVals, noiseX, noiseY)
     if not pos.x then error("No position argument!") end
     if not noiseVals then error("No chunk argument!") end
