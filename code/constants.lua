a03cd3a55dac1dfb50a52515ae22570c2a63f955
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-05-04 21:38:44 +0300

    task queue length debug

--- code/constants.lua
+++ code/constants.lua
@@ -2,8 +2,8 @@ local constants = {}
 
 constants.MONTHS_IN_A_YEAR = 2
 constants.STEPS_IN_A_MONTH = 1
-constants.SECONDS_IN_A_STEP = 10
-constants.CHUNKS_PROCESSED_PER_TICK = 2
+constants.SECONDS_IN_A_STEP = 15
+constants.CHUNKS_PROCESSED_PER_TICK = 1
 constants.TOP_ELEVATION = 35
 constants.VORONOI_FRAME_SIZE = 130
 
--- code/season-step.lua
+++ code/season-step.lua
@@ -1,11 +1,15 @@
 local taskQueueAdd = require "code/procedures/tasks-add"
 local printStepDebugData = require "code/debug/print-step-debug-data.lua"
 
+local taskQueue = require "code/persistence/tasks-queue"
+
 return function(time)
+    game.print("before step taskQueue::length "..taskQueue:length())
     for chunk in game.surfaces.nauvis.get_chunks() do
         if game.surfaces.nauvis.is_chunk_generated(chunk) then
             taskQueueAdd(time, chunk)
-            printStepDebugData(time)
         end
     end
+    game.print("after step taskQueue::length "..taskQueue:length())
+    printStepDebugData(time)
 end
