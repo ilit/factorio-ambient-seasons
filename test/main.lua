3c4ebc0260b5383d94a5e1798a190857fb09f58f
Your Name <you@example.com>
2022-05-07 16:46:47 +0300

    rename

--- test/main.lua
+++ test/main.lua
@@ -2,6 +2,6 @@ EXPORT_ASSERT_TO_GLOBALS = true
 luaunit = require('test/luaunit')
 
 TestElevationToStep = require("test/elevation-to-step-test")
-TestChunkPositionsTwoArrays = require("test/chunk-to-positions-array-test")
+TestChunkPositionsTwoArrays = require("chunk-to-positions-xs-xy-test")
 
 os.exit( luaunit.LuaUnit.run("-v") )
