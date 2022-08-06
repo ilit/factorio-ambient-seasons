f97d3445a59bc1a75b16fbcb143640cf23536e44
Your Name <you@example.com>
2022-08-06 22:33:42 +0300

    stop biome caching on chunk generation

--- control.lua
+++ control.lua
@@ -1,18 +1,10 @@
 local seasonProgress = require "code/season-progress"
 local tasksQueueExec = require "code/procedures/tasks-execute"
-local saveChunkBiomes = require "code/procedures/save-chunk-biomes"
 
 local registerEvents = function()
     script.on_event(defines.events.on_tick, function(_)
         seasonProgress()
         tasksQueueExec()
-    end)
-
-    --- Not called for start area chunks
-    script.on_event(defines.events.on_chunk_generated, function(event)
-        if event.surface.index == 1 and game then
-            saveChunkBiomes(event.position)
-        end
     end)
 end
 
